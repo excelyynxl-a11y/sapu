9eb88d19eb5fcb4253d4c8891b2d2ae5e6948ce8
Excelyynx <elim0079@student.monash.edu>
2026-08-07 20:23:50 +0800

    feat(axios): configure axiosInstance

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000" : "/api",
+})
+
+// attach JWT token to every request
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
