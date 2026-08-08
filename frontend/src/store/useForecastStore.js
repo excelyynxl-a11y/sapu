c84db50a6e065ed592aef39f2fb14dafab07e510
Excelyynx <elim0079@student.monash.edu>
2026-08-08 20:06:38 +0800

    feat(store): useForecastStore api calls to get forecast, get risk period

--- frontend/src/store/useForecastStore.js
+++ frontend/src/store/useForecastStore.js
@@ -1,18 +1,49 @@
 import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios.js';
 
-export const useForecastStore = create((set, get) => ({
-    
+export const useForecastStore = create((set) => ({
+    window_days: null,
     starting_balance: null,
     threshold: null,
     forecastList: [],
     riskPeriodList: [],
 
-    getMyForecast: () => {
-
+    getMyForecast: async (window_days, starting_balance, threshold) => {
+        try {
+            const res = await axiosInstance.get("/forecast/me", {
+                params: { window_days, starting_balance, threshold }
+            });
+            set({
+                window_days,
+                starting_balance,
+                threshold,
+                forecastList: res.data.balance_series,
+            });
+        } catch (error) {
+            console.log("Error in getMyForecast:", error);
+            throw error;
+        }
     },
 
-    getMyRiskPeriod: () => {
-        
-    }
+    getMyRiskPeriod: async (window_days, starting_balance, threshold) => {
+        try {
+            const res = await axiosInstance.get("/forecast/me/risk", {
+                params: { window_days, starting_balance, threshold }
+            });
+            set({
+                riskPeriodList: res.data.risk_periods,
+            });
+        } catch (error) {
+            console.log("Error in getMyRiskPeriod:", error);
+            throw error;
+        }
+    },
+
+    clearForecast: () => set({
+        window_days: null,
+        starting_balance: null,
+        threshold: null,
+        forecastList: [],
+        riskPeriodList: [],
+    })
 }))
