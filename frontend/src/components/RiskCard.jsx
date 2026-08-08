e6ecd4029b533db2221de77b7e621a05bf8609d8
Excelyynx <elim0079@student.monash.edu>
2026-08-08 16:16:07 +0800

    feat(card): RiskCard skeleton rendered in Forecast 'Risk Summary' section

---
+++ frontend/src/components/RiskCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function RiskCard({
+    start_date,
+    end_date,
+    min_balance,
+    days_below_threshold
+}) {
+  return (
+
+    <div>
+        <div>
+            {/* start date */}    
+                    
+        </div>
+
+        <div>
+            {/* end date */}
+        </div>
+
+        <div>
+            {/* minimum balance during this time frame */}
+        </div>
+
+        <div>
+            {/* danger day count */}
+        </div>
+    </div>
+  )
+}
+
+export default RiskCard
