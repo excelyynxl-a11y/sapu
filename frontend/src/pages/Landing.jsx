cd26958f648832c2087f2043a769db7d97e8a6d7
Excelyynx <elim0079@student.monash.edu>
2026-08-08 21:28:00 +0800

    todo(landing): Landing skeleton

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div>
+      {/* SAPU header with sapu.png logo */}
+      <div>
+        SAPU 
+      </div>
+
+      <div>
+        Plug in your income and recurring bills, and see your projected balance across the month so you know when things get tight.
+      </div>
+
+      <div>
+        Get started 
+
+        {/* signin and register buttons */}
+        <div>
+          <div>
+            Sign in 
+          </div>
+          <div>
+            Register 
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
