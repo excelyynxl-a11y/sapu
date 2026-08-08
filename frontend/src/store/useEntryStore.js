3ebff7c2b692cf97c9f63eeea9f9a7f675a824e9
Excelyynx <elim0079@student.monash.edu>
2026-08-08 16:11:26 +0800

    fix(import): import create, axiosInstance

--- frontend/src/store/useEntryStore.js
+++ frontend/src/store/useEntryStore.js
@@ -1,3 +1,5 @@
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
 
 export const useEntryStore = create((set, get) => ({
     entries: [],
--- frontend/src/store/useForecastStore.js
+++ frontend/src/store/useForecastStore.js
@@ -1,8 +1,11 @@
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
 
 export const useForecastStore = create((set, get) => ({
     
+    starting_balance: null,
+    threshold: null,
     forecastList: [],
-    
     riskPeriodList: [],
 
     getMyForecast: () => {
